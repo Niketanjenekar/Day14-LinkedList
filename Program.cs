ac11962ce07a53685a796164a0cdc4ace5f1ee3b
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-03-30 00:39:25 +0530

    Creation of Linked-List Usecase1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Day14_LinkedlistProblem
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcom to the Linked List Program");
+            Linkedlist linkedList = new Linkedlist();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            Console.WriteLine("**********************************************************");
+        }
+    }
+}
